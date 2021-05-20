b207a6530ca05c649df6e691a3b4000eaaa98a3c
Antoine Rebillard <antoine.rebillard@pm.me>
2021-05-20 09:15:13 +0200

    updated config

--- config/settings/production.py
+++ config/settings/production.py
@@ -22,6 +22,8 @@ ALLOWED_HOSTS = ['.herokuapp.com']
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#secure-ssl-redirect
 SECURE_SSL_REDIRECT = True
+# https://docs.djangoproject.com/en/dev/ref/settings/#secure-proxy-ssl-header
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # https://docs.djangoproject.com/en/dev/ref/settings/#session-cookie-secure
 SESSION_COOKIE_SECURE = True
 # https://docs.djangoproject.com/en/dev/ref/settings/#csrf-cookie-secure
